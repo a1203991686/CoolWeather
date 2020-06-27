0dcb03a0957d6ef3722296c75bb7c21d28998427
a1203991686 <a1203991686@126.com>
2020-06-27 22:17:31 +0800

    ♻️optimization:replace to dataBinding in ChooseLiveDataFragment

--- 3/SunnyWeather/app/src/main/java/com/littlecorgi/sunnyweather/ui/livedata/ChooseLiveDataFragment.kt
+++ 3/SunnyWeather/app/src/main/java/com/littlecorgi/sunnyweather/ui/livedata/ChooseLiveDataFragment.kt
@@ -7,23 +7,26 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 import com.littlecorgi.sunnyweather.R
-import kotlinx.android.synthetic.main.fragment_choose_live_data.*
+import com.littlecorgi.sunnyweather.databinding.FragmentChooseLiveDataBinding
 
 class ChooseLiveDataFragment : Fragment() {
+
+    private lateinit var binding: FragmentChooseLiveDataBinding
+
+    private val navController = NavHostFragment.findNavController(this)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_choose_live_data, container, false)
+        binding = FragmentChooseLiveDataBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val navController = NavHostFragment.findNavController(this)
-
-        toLiveDataFragmentBtn.setOnClickListener {
+        binding.toLiveDataFragmentBtn.setOnClickListener {
             navController.navigate(R.id.action_changeLiveDataFragment_to_liveDataFragment)
         }
     }
